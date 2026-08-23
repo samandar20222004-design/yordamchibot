324ad1a2f5f68d398edde46b2d4c65b93d374532
samandar20222004-design <samandar20222004@gmail.com>
2026-08-23 11:27:30 +0500

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .start import start_command, list_channels_handler, add_channel_start, add_channel_process, WAITING_CHANNEL_FORWARD
-from .new_post import new_post_start, choose_channel_step, post_content_step, post_time_step, confirm_post_step, cancel_handler, CHOOSE_CHANNEL, POST_CONTENT, POST_TIME, CONFIRM_POST
-from .list_posts import list_posts_command
-from .delete_post import delete_post_start, delete_post_process, WAIT_DELETE_ID
-from .admin import admin_panel_handler, broadcast_start, broadcast_send, ADMIN_BROADCAST_STATE
