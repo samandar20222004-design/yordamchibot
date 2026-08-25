968700b87710c446f1e82e7217af569c0a9eb301
samandar20222004-design <samandar20222004@gmail.com>
2026-08-25 16:37:36 +0500

    Update config.py

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,15 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SUPER_ADMIN_RAW = os.getenv("ADMIN_ID", "0")
-
-try:
-    SUPER_ADMIN = int(SUPER_ADMIN_RAW)
-except ValueError:
-    SUPER_ADMIN = 0
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN topilmadi! .env faylida BOT_TOKEN ni belgilang.")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7105264103"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+PORT = int(os.getenv("PORT", 8080))
