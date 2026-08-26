df135b5418bc7d242e4f4615f1e5bf4a74c2f049
samandar20222004-design <samandar20222004@gmail.com>
2026-08-26 09:38:59 +0500

    Update config.py

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7105264103"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 DATABASE_URL = os.getenv("DATABASE_URL")
-PORT = int(os.getenv("PORT", 8080))
+PORT = int(os.getenv("PORT", 10000))
